85138a4b28c5ed6c359b7a7872d06242b753c3f4
Quentin Chavigny--Turbe <qct@laposte.net>
2024-07-15 16:05:25 +0200

    in progress

---
+++ examples/detect_multi_per_zone_spi/src/main.rs
@@ -0,0 +1,147 @@
+#![no_std]
+#![no_main]
+
+use vl53l8cx::{
+    Vl53l8cx,
+    ResultsData
+};
+
+use panic_halt as _; 
+use cortex_m_rt::entry;
+
+use core::{fmt::Write, cell::RefCell};
+
+use stm32f4xx_hal::{
+    gpio::{
+        Output, 
+        Pin, 
+        PinState::High,
+        gpioa, 
+        gpiob,
+        Alternate}, 
+    pac::{USART2, Peripherals, CorePeripherals, TIM1}, 
+    prelude::*, 
+    serial::{Config, Tx}, 
+    timer::{SysDelay, Delay},
+    rcc::{Rcc, Clocks}
+};
+
+// SPI related imports
+use stm32f4xx_hal::spi::Spi;
+use stm32f4xx_hal::pac::SPI1;
+use embedded_hal::spi::MODE_3;
+use embedded_hal_bus::spi::{NoDelay, RefCellDevice};
+
+fn write_results(tx: &mut Tx<USART2>, results: &ResultsData, width: usize) {
+
+    writeln!(tx, "\x1B[2H").unwrap();
+
+    writeln!(tx, "VL53L8A1 Double Target Detection application\n").unwrap();
+    writeln!(tx, "Cell Format :\n").unwrap();
+    writeln!(
+        tx, 
+        "\x1b[96m{dis:>20}\x1b[0m \x1b[96m{dis2:<20}\x1b[0m", 
+        dis="Distance [mm]", 
+        dis2="Distance (target 2)"
+    ).unwrap();
+    writeln!(
+        tx, 
+        "\x1b[93m{sig:>20}\x1b[0m \x1b[93m{sig2:<20}\x1b[0m", 
+        sig="Signal [kcps/spad]", 
+        sig2="Signal (target 2)"
+    ).unwrap();
+
+    for j in (0..(width*2)).step_by(2) {
+        for _ in 0..width { write!(tx, "+-----------").unwrap(); } writeln!(tx, "+").unwrap();
+        for i in (0..(width*2)).step_by(2) {
+            write!(
+                tx, 
+                "|\x1b[96m{dis:>5}\x1b[0m \x1b[96m{dis2:<5}\x1b[0m", 
+            dis=results.distance_mm[width*j+i], 
+            dis2=results.distance_mm[width*j+i+1]
+            ).unwrap();
+        } write!(tx, "|\n").unwrap();
+
+        for i in (0..(width*2)).step_by(2) {
+            let mut sig: u32 = results.signal_per_spad[width*j+i];
+            let mut sig2: u32 = results.signal_per_spad[width*j+i+1];
+            if sig > 9999 { sig = 9999; }
+            if sig2 > 9999 { sig2 = 9999; }
+            write!(
+                tx, 
+                "|\x1b[93m{sig:>5}\x1b[0m \x1b[93m{sig2:<5}\x1b[0m", 
+                sig=sig, 
+                sig2=sig2
+            ).unwrap();
+        } write!(tx, "|\n").unwrap();
+        
+    }
+    for _ in 0..width { write!(tx, "+-----------").unwrap(); } writeln!(tx, "+").unwrap();
+
+}
+
+const WIDTH: usize = 4;
+
+#[entry]
+fn main() -> ! {
+    let mut results: ResultsData = ResultsData::new();
+    
+    let dp: Peripherals = Peripherals::take().unwrap();
+    let cp: CorePeripherals = CorePeripherals::take().unwrap();
+    let rcc: Rcc = dp.RCC.constrain();
+    let clocks: Clocks = rcc.cfgr.use_hse(8.MHz()).sysclk(48.MHz()).freeze();
+    let tim_top: Delay<TIM1, 1000> = dp.TIM1.delay_ms(&clocks);
+    let _delay: SysDelay = cp.SYST.delay(&clocks);
+
+    let gpioa: gpioa::Parts = dp.GPIOA.split();
+    let gpiob: gpiob::Parts = dp.GPIOB.split();
+    
+    let _pwr_pin: Pin<'A', 7, Output> = gpioa.pa7.into_push_pull_output_in_state(High);
+    let lpn_pin: Pin<'B', 0, Output> = gpiob.pb0.into_push_pull_output_in_state(High);
+    let tx_pin: Pin<'A', 2, Alternate<7>> = gpioa.pa2.into_alternate();
+    
+    let mut tx: Tx<USART2> = dp.USART2.tx(
+        tx_pin,
+        Config::default()
+        .baudrate(460800.bps())
+        .wordlength_8()
+        .parity_none(),
+        &clocks).unwrap();
+    
+    let resolution: u8 = (WIDTH * WIDTH) as u8;
+
+    let sclk: Pin<'B', 3, Alternate<5>> = gpiob.pb3.into_alternate().internal_pull_up(true);
+    let miso: Pin<'B', 4, Alternate<5>> = gpiob.pb4.into_alternate();
+    let mosi: Pin<'B', 5, Alternate<5>> = gpiob.pb5.into_alternate();
+    let cs_pin: Pin<'B', 6, Output> = gpiob.pb6.into_push_pull_output_in_state(High);
+
+    let spi: Spi<SPI1> = Spi::new(
+        dp.SPI1,
+        (sclk, miso, mosi),
+        MODE_3,
+        3.MHz(),
+        &clocks,);
+        
+    let spi: RefCell<Spi<SPI1>> = RefCell::new(spi);
+    let spi: RefCellDevice<Spi<SPI1>, Pin<'B', 6, Output>, NoDelay> = RefCellDevice::new_no_delay(&spi, cs_pin).unwrap();
+    
+    let mut sensor = Vl53l8cx::new_spi(
+        spi, 
+        lpn_pin,
+        tim_top
+    ).unwrap();
+
+    sensor.init_sensor().unwrap();
+    sensor.set_resolution(resolution).unwrap();
+    sensor.set_frequency_hz(30).unwrap();
+    sensor.start_ranging().unwrap();
+
+
+    write_results(&mut tx, &results, WIDTH);
+    
+    loop {
+        while !sensor.check_data_ready().unwrap() {} // Wait for data to be ready
+        results = sensor.get_ranging_data().unwrap(); // Get and parse the result data
+        write_results(&mut tx, &results, WIDTH); // Print the result to the output
+    }   
+}
