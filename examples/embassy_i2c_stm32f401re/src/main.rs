d8f674f7ec5b0e7a8ec30b3e09e502efd53c4d7e
Quentin Chavigny--Turbe <qct@laposte.net>
2024-08-26 11:57:17 +0200

    in progress

--- examples/embassy_i2c_stm32f401re/src/main.rs
+++ examples/embassy_i2c_stm32f401re/src/main.rs
@@ -1,142 +1,85 @@
-
 #![no_std]
 #![no_main]
 
 use cortex_m_rt::entry;
-use embassy_stm32::gpio::{Level, Speed, Output, Input, Pin};
-use embassy_stm32::gpio::{AnyPin, OutputOpenDrain};
-use embassy_stm32::i2c::{I2c, Config as I2cConfig};
-use embassy_stm32::usart::{BufferedUartTx, Config as UartConfig, UartTx};
-use embassy_stm32::time::mhz;
+use embassy_stm32::bind_interrupts;
+use embassy_stm32::dma::NoDma;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::i2c::{self, Config as I2cConfig, I2c};
+use embassy_stm32::peripherals::{self, USART2};
+use embassy_stm32::rcc::{Config as RccConfig, Hse, HseMode};
+use embassy_stm32::time::{khz, mhz};
+use embassy_stm32::usart::{self, BufferedInterruptHandler, BasicInstance, Config as UsartConfig, DataBits, Parity, Uart, UartTx};
+use embassy_stm32::Config;
+use embassy_time::Delay;
+use cortex_m::asm::delay;
+use core::fmt::Write;
+use heapless::String;
+use defmt::*;
+use {defmt_rtt as _, panic_probe as _};
+// use core::fmt::{self, Write};
+// use vl53l8cx::Vl53l8cx;
 
-use embassy_stm32::peripherals::USART2;
-use embassy_stm32::Peripherals;
-use embassy_time::{Duration, Timer};
-use embedded_hal::delay::{DelayNs};
-use embassy_stm32::rcc::{Clocks};
-use embassy_stm32::time::Hertz;
-use vl53l8cx::{Vl53l8cx, consts::VL53L8CX_DEFAULT_I2C_ADDRESS, ResultsData};
-use core::{fmt::Write, cell::RefCell};
-
-
-// fn write_results(tx: &mut BufferedUartTx<'_, USART2>, results: &ResultsData, width: usize) {    
-//     write!(tx, "\x1B[2H").unwrap();
-
-//     writeln!(tx, "VL53L8A1 Simple Ranging demo application\n").unwrap();
-
-//     writeln!(tx, "Cell Format :\n").unwrap();
-//     writeln!(
-//         tx, 
-//         "\x1b[96m{dis:>20}\x1b[0m \x1b[92m{sta:<20}\x1b[0m", 
-//         dis="Distance [mm]", 
-//         sta="Status"
-//     ).unwrap();
-//     writeln!(
-//         tx, 
-//         "\x1b[93m{sig:>20}\x1b[0m \x1b[91m{amb:<20}\x1b[0m", 
-//         sig="Signal [kcps/spad]", 
-//         amb="Ambient [kcps/spad]"
-//     ).unwrap();
-
-//     for j in 0..width {
-//         for _ in 0..width { write!(tx, "+----------").unwrap(); } writeln!(tx, "+").unwrap();
-        
-//         #[cfg(not(any(feature="VL53L8CX_DISABLE_DISTANCE_MM", feature="VL53L8CX_DISABLE_TARGET_STATUS")))]
-//         {
-//             for i in 0..width {
-//                 write!(
-//                     tx, 
-//                     "|\x1b[96m{dis:>5}\x1b[0m \x1b[92m{sta:<4}\x1b[0m", 
-//                 dis=results.distance_mm[width*j+i], 
-//                 sta=results.target_status[width*j+i]
-//                 ).unwrap();
-//             } write!(tx, "|\n").unwrap();
-//         }
-
-//         #[cfg(not(any(feature="VL53L8CX_DISABLE_SIGNAL_PER_SPAD", feature="VL53L8CX_DISABLE_AMBIENT_PER_SPAD")))]
-//         {
-//             for i in 0..width {
-//                 let mut sig: u32 = results.signal_per_spad[width*j+i];
-//                 if sig > 9999 { sig = 9999; }
-//                 write!(
-//                     tx, 
-//                     "|\x1b[93m{sig:>5}\x1b[0m \x1b[91m{amb:<4}\x1b[0m", 
-//                     sig=sig, 
-//                     amb=results.ambient_per_spad[width*j+i]
-//                 ).unwrap();
-//             } write!(tx, "|\n").unwrap();
-//         }
-//     }
-//     for _ in 0..width { write!(tx, "+----------").unwrap(); } writeln!(tx, "+").unwrap();
-// }
-
+bind_interrupts!(struct Irqs {
+    USART2 => BufferedInterruptHandler<USART2>;
+    // I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
+    // I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
+});
 
 #[entry]
 fn main() -> ! {
-   // Safely take ownership of device peripherals
-   let p = Peripherals::take().unwrap();
 
-   // Configure RCC (Reset and Clock Control)
-   let rcc = Rcc::new(p.RCC);
-   let clocks = rcc.cfgr()
-       .use_hse(Hertz(8_000_000))  // Use HSE at 8 MHz
-       .sysclk(Hertz(48_000_000))  // Set system clock to 48 MHz
-       .freeze();
+    let mut rcc_config = RccConfig::default();
+    let hse = Hse {freq: mhz(8), mode: HseMode::Oscillator};
+    rcc_config.hse = Some(hse);
 
-   // Initialize the timer (TIM1) with the current clock settings
-   let mut tim_top = Timer::tim1(p.TIM1, clocks);
+    let mut config = Config::default();
+    config.rcc = rcc_config;
 
-    let mut results: ResultsData = ResultsData::new();
+    let p = embassy_stm32::init(config);
 
-    // UART setup
-    // let tx_pin = p.PA2;
+    let mut usart_config = UsartConfig::default();
+    usart_config.baudrate = 460800;
+    usart_config.data_bits = DataBits::DataBits8;
+    usart_config.parity = Parity::ParityNone;
 
-    // let mut tx = UartTx::new(
-    //     p.USART2,
-    //     tx_pin,
-    //     Default::default(),
-    //     UartConfig::default()
-    // );
+    let mut tx = UartTx::new(
+        p.USART2,
+        p.PA2, 
+        NoDma,
+        usart_config).unwrap();
 
-    // I2C setup
-    let scl = dp.PB8;
-    let sda = dp.PB9;
 
-    let i2c = I2c::new(
-        dp.I2C1,
-        scl,
-        sda,
-        I2cConfig::default()
-    );
+    // let _pwr_pin = Output::new(p.PA7, Level::High, Speed::Low);
+    
+    // let mut lpn_pin = Output::new(p.PB0, Level::High, Speed::Low);
 
-    let lpn_pin = Output::new(
-        dp.PB0, 
-        Level::High, 
-        Speed::Low);
-        
-    let lpn_pin = Output::new(
-        dp.PA7, 
-        Level::High, 
-        Speed::Low);    
+    // let tim: Delay = Delay {};
 
-    let delay = Timer::after(Duration::from_millis(100));
+    // let i2c = I2c::new(
+    //     p.I2C1, 
+    //     p.PB8, 
+    //     p.PB9, 
+    //     Irqs, 
+    //     NoDma, 
+    //     NoDma, 
+    //     khz(400), 
+    //     I2cConfig::default());
 
-    let mut tim_top = Timer::new_tim1(dp.TIM1, rcc.clocks);
 
-    let address = VL53L8CX_DEFAULT_I2C_ADDRESS;
+    // let mut sensor = Vl53l8cx::new_i2c(i2c, lpn_pin, tim).unwrap();
 
-    let mut sensor_top = Vl53l8cx::new_i2c(
-        i2c, 
-        lpn_pin, 
-        tim_top).unwrap();
+    // sensor.init_sensor(0x29).unwrap();
+    // sensor.start_ranging().unwrap();
 
-    sensor_top.init_sensor(address).unwrap(); 
-    sensor_top.set_frequency_hz(30).unwrap();
-    sensor_top.start_ranging().unwrap();
-
+    let mut n: u8 = 0;
+    let mut msg: String<16> = String::new();
     loop {
-        while !sensor_top.check_data_ready().unwrap() {} // Wait for data to be ready
-        results = sensor_top.get_ranging_data().unwrap(); // Get and parse the result data
-        // write_results(&mut tx, &results, 4); // Print the result to the output
+        // while !sensor.check_data_ready().unwrap() {}
+        // let results = sensor.get_ranging_data().unwrap();
+        core::writeln!(&mut msg, "hello {}", n).unwrap();
+        let _ = tx.blocking_write(msg.as_bytes());
+        n += 1;
+        msg.clear();
     }
 }
